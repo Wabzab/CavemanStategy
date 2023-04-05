a4aef8dbc68ad14b251b2e2c9e692d50afe5596a
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-04-05 09:56:45 +0200

    Expand debug UI
    Added more options to play around with the temp map during run time.
    Also a text view to see the seed.
    Temp map and Height map now use the same seed.

--- scripts/map_handler.gd
+++ scripts/map_handler.gd
@@ -1,12 +1,18 @@
 extends Node
 
 @onready var map_node: Node2D = $MapNode
+@onready var lbl_seed: Label = $Camera2D/UI/VBC/HBC/PC/VBC/Seed
 @onready var map_size_node: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/MapSize/SpinBox
-@onready var noise_type_node: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/NoiseType/SpinBox
-@onready var fractal_type_node: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/FractalType/SpinBox
-@onready var freqeuncy_node: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Frequency/SpinBox
-@onready var globals = get_node("/root/Globals")
+# Height Map vars
+@onready var height_noise_type: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/NoiseType/SpinBox
+@onready var height_fractal_type: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/FractalType/SpinBox
+@onready var height_freqeuncy_limit: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Frequency/SpinBox
+# Temp Map vars
+@onready var temp_noise_type: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/NoiseType2/SpinBox
+@onready var temp_fractal_type: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/FractalType2/SpinBox
+@onready var temp_freqeuncy_limit: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Frequency2/SpinBox
 
+@onready var globals = get_node("/root/Globals")
 @onready var hex_scene = preload("res://scenes/hex_tile.tscn")
 
 const max_size: int = 100
@@ -17,13 +23,16 @@ const max_size: int = 100
 @export_range(0,1) var mid_water : float = 0.3 
 @export_range(0,1) var shallow_water : float = 0.45
 @export_range(0,100) var flower_chance : int = 25
-@export_range(0,5) var noise_type : int = 0
-@export_range(0,3) var fractal_type : int = 0
-@export_range(1, 100) var frequency : int = 1
+@export_range(0,5) var height_noise : int = 0
+@export_range(0,3) var height_fractal : int = 0
+@export_range(1, 100) var height_frequency : int = 1
 @export_group("Temperature Map Settings")
 @export_range(0,1) var snow: float = 0.1
 @export_range(0,1) var tundra: float = 0.3
 @export_range(0,1) var grassland: float = 0.7
+@export_range(0,5) var temp_noise : int = 0
+@export_range(0,3) var temp_fractal : int = 0
+@export_range(1, 100) var temp_frequency : int = 1
 @export_group("Hexagon Settings")
 @export var tile_width: int = 126
 @export var tile_height: int = 144
@@ -59,19 +68,21 @@ func InitialiseMap(size: int):
 	map_node.scale = Vector2(map_scale, map_scale)
 
 func GenerateNewMap(size: int):
-	var height_map: Image = await GenerateHeightMap(size)
-	var temp_map: Image = await GenerateTemperatureMap(size)
+	var seed = randi()
+	lbl_seed.text = "Seed: " + str(seed)
+	var height_map: Image = await GenerateHeightMap(size, seed)
+	var temp_map: Image = await GenerateTemperatureMap(size, seed)
 	var world_map: Array = EvaluateWorldMap(height_map, temp_map, size)
 	return world_map
 
-func GenerateHeightMap(size: int):
+func GenerateHeightMap(size: int, seed: int):
 	# ----- || Gen Height Map || ----- #
 	var height_map = NoiseTexture2D.new()
 	height_map.noise = FastNoiseLite.new()
-	height_map.noise.seed = randi()
-	height_map.noise.set_noise_type(noise_type)
-	height_map.noise.set_fractal_type(fractal_type)
-	height_map.noise.set_frequency(frequency/100.0)
+	height_map.noise.seed = seed
+	height_map.noise.set_noise_type(height_noise)
+	height_map.noise.set_fractal_type(height_fractal)
+	height_map.noise.set_frequency(height_frequency/100.0)
 	height_map.width = size
 	height_map.height = size
 	await height_map.changed
@@ -79,11 +90,14 @@ func GenerateHeightMap(size: int):
 	return height_image
 	# ----- ||    Complete    || ----- #
 
-func GenerateTemperatureMap(size: int):
+func GenerateTemperatureMap(size: int, seed: int):
 	# ----- || Gen Temp Map || ----- #
 	var temp_map = NoiseTexture2D.new()
 	temp_map.noise = FastNoiseLite.new()
-	temp_map.noise.seed = randi()
+	temp_map.noise.seed = seed
+	temp_map.noise.set_noise_type(temp_noise)
+	temp_map.noise.set_fractal_type(temp_fractal)
+	temp_map.noise.set_frequency(temp_frequency/100.0)
 	temp_map.width = size
 	temp_map.height = size
 	await temp_map.changed # causing freed object emitting signal error? prob engine issue
@@ -146,14 +160,23 @@ func DrawMap():
 			index += 1
 
 func ClearMap():
+	# Clear map by setting all tiles to null
+	# This makes them recognised as "useless"
 	for i in range(map_node.get_child_count()):
 		map_node.get_child(i).SetTile(globals.TileType.NULL, globals)
 
 
 func _on_button_pressed():
+	# Adjust map size
 	map_size = clamp(map_size_node.value, 10, max_size)
-	noise_type = noise_type_node.value
-	fractal_type = fractal_type_node.value
-	frequency = freqeuncy_node.value
+	# Adjust height map settings
+	height_noise = height_noise_type.value
+	height_fractal = height_fractal_type.value
+	height_frequency = height_freqeuncy_limit.value
+	# Adjust temp map settings
+	temp_noise = temp_noise_type.value
+	temp_fractal = temp_fractal_type.value
+	temp_frequency = temp_freqeuncy_limit.value
+	# Reload Map
 	ClearMap()
 	InitialiseMap(map_size)
