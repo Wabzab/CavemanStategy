3ff356aba226d2471a51c3d571007b2e694a003b
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-04-07 19:45:00 +0200

    To Rock and Stone!
    Implemented hills and mountains to map generation.
    Expanded debug UI to handle most map settings.

--- scripts/hex_tile.gd
+++ scripts/hex_tile.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
 
+@onready var feature: Sprite2D = $feature
 var neighbours = []
 
 func SetTile(tile_type, globals):
@@ -7,22 +8,51 @@ func SetTile(tile_type, globals):
 	match tile_type:
 		globals.TileType.OCEAN_DEEP:
 			texture = load("res://assets/hextiles/ocean_deep.png")
+			feature.texture = null
 		globals.TileType.OCEAN_MID:
 			texture = load("res://assets/hextiles/ocean_mid.png")
+			feature.texture = null
 		globals.TileType.OCEAN_SHALLOW:
 			texture = load("res://assets/hextiles/ocean_shallow.png")
-		globals.TileType.GRASSLAND:
+			feature.texture = null
+		globals.TileType.GRASS:
 			texture = load("res://assets/hextiles/grassland.png")
-		globals.TileType.GRASSLAND_FLOWER:
+			feature.texture = null
+		globals.TileType.GRASS_FLOWER:
 			texture = load("res://assets/hextiles/grassland_flower.png")
-		globals.TileType.DESERT:
+			feature.texture = null
+		globals.TileType.GRASS_HILL:
+			texture = load("res://assets/hextiles/grassland.png")
+			feature.texture = load("res://assets/features/grass_hill.png")
+		globals.TileType.GRASS_MOUNTAIN:
+			texture = load("res://assets/hextiles/grassland.png")
+			feature.texture = load("res://assets/features/mountain.png")
+		globals.TileType.SAND:
 			texture = load("res://assets/hextiles/sand.png")
+			feature.texture = null
+		globals.TileType.SAND_HILL:
+			texture = load("res://assets/hextiles/sand.png")
+			feature.texture = load("res://assets/features/sand_hill.png")
+		globals.TileType.SAND_MOUNTAIN:
+			texture = load("res://assets/hextiles/sand.png")
+			feature.texture = load("res://assets/features/mountain.png")
 		globals.TileType.SNOW:
 			texture = load("res://assets/hextiles/snow.png")
+			feature.texture = null
+		globals.TileType.SNOW_HILL:
+			texture = load("res://assets/hextiles/snow.png")
+			feature.texture = load("res://assets/features/snow_hill.png")
+		globals.TileType.SNOW_MOUNTAIN:
+			texture = load("res://assets/hextiles/snow.png")
+			feature.texture = load("res://assets/features/mountain.png")
 		globals.TileType.TUNDRA:
 			texture = load("res://assets/hextiles/tundra.png")
+			feature.texture = null
 		globals.TileType.NULL:
-			texture = load("res://assets/hextiles/Purple tile.png")
+			texture = null
+			feature.texture = null
 		_:
+			texture = load("res://assets/hextiles/Purple tile.png")
+			feature.texture = null
 			print("Tile could not be found!")
 
--- scripts/map_handler.gd
+++ scripts/map_handler.gd
@@ -7,10 +7,18 @@ extends Node
 @onready var tr_tempmap: TextureRect = $Camera2D/UI/VBC/HBC/Maps/VBoxContainer/tr_tempmap
 @onready var tr_blendimage: TextureRect = $Camera2D/UI/VBC/HBC/Maps/VBoxContainer/tr_blendimage
 # Height Map vars
+@onready var deep_water_max: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Deep/SpinBox
+@onready var mid_water_max: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Mid/SpinBox
+@onready var shallow_water_max: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Shallow/SpinBox
+@onready var mountain_min: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Mountain/SpinBox
+@onready var hill_min: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Hill/SpinBox
 @onready var height_noise_type: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/NoiseType/SpinBox
 @onready var height_fractal_type: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/FractalType/SpinBox
 @onready var height_freqeuncy_limit: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Frequency/SpinBox
 # Temp Map vars
+@onready var snow_max: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Snow/SpinBox
+@onready var tundra_max: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Tundra/SpinBox
+@onready var grass_max: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Grass/SpinBox
 @onready var temp_noise_type: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/NoiseType2/SpinBox
 @onready var temp_fractal_type: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/FractalType2/SpinBox
 @onready var temp_freqeuncy_limit: SpinBox = $Camera2D/UI/VBC/HBC/PC/VBC/Frequency2/SpinBox
@@ -19,23 +27,25 @@ extends Node
 @onready var hex_scene = preload("res://scenes/hex_tile.tscn")
 
 const max_size: int = 100
-@export_range(10, max_size) var map_size : int = 10
-@export var map_scale : float = 1
+@export_range(10, max_size) var map_size : int = 50
+@export var map_scale : float = 0.5
 @export_group("Height Map Settings")
 @export_range(0,1) var deep_water : float = 0.1 
 @export_range(0,1) var mid_water : float = 0.3 
 @export_range(0,1) var shallow_water : float = 0.45
+@export_range(0,1) var hill : float = 0.80
+@export_range(0,1) var mountain : float = 0.90
 @export_range(0,100) var flower_chance : int = 25
-@export_range(0,5) var height_noise : int = 0
-@export_range(0,3) var height_fractal : int = 0
-@export_range(1, 100) var height_frequency : int = 1
+@export_range(0,5) var height_noise : int = 2
+@export_range(0,3) var height_fractal : int = 1
+@export_range(1, 100) var height_frequency : int = 5
 @export_group("Temperature Map Settings")
 @export_range(0,1) var snow: float = 0.1
 @export_range(0,1) var tundra: float = 0.3
-@export_range(0,1) var grassland: float = 0.7
-@export_range(0,5) var temp_noise : int = 0
-@export_range(0,3) var temp_fractal : int = 0
-@export_range(1, 100) var temp_frequency : int = 1
+@export_range(0,1) var grass: float = 0.7
+@export_range(0,5) var temp_noise : int = 1
+@export_range(0,3) var temp_fractal : int = 3
+@export_range(1, 100) var temp_frequency : int = 5
 @export_group("Hexagon Settings")
 @export var tile_width: int = 126
 @export var tile_height: int = 144
@@ -125,30 +135,60 @@ func EvaluateWorldMap(height_map: Image, temp_map: Image, size: int):
 	# ----- | Done! | ----- #
 	
 	# ----- Evaluate World ----- #
+	var tile_type = globals.TileType.NULL
 	for x in range(size):
 		for y in range(size):
 			var height = height_map.get_pixel(x, y).r
 			var temp = temp_map.get_pixel(x, y).r
-			
-			if height < deep_water:
-				world_map[x][y] = globals.TileType.OCEAN_DEEP
-			elif height < mid_water:
-				world_map[x][y] = globals.TileType.OCEAN_MID
-			elif height < shallow_water:
-				world_map[x][y] = globals.TileType.OCEAN_SHALLOW
-			elif temp < snow:
-				world_map[x][y] = globals.TileType.SNOW
-			elif temp < tundra:
-				world_map[x][y] = globals.TileType.TUNDRA
-			elif temp < grassland:
-				if randi()%100 < flower_chance:
-					world_map[x][y] = globals.TileType.GRASSLAND_FLOWER
-				else:
-					world_map[x][y] = globals.TileType.GRASSLAND
+			if height < shallow_water:
+				tile_type = DetermineOceanBiome(height, temp)
 			else:
-				world_map[x][y] = globals.TileType.DESERT
+				tile_type = DetermineLandBiome(height, temp)
+			world_map[x][y] = tile_type
+			
 	return world_map
 	# ----- |    Done    | ----- #
+
+func DetermineOceanBiome(height: float, temp: float):
+	# Determine ocean biome #
+	if height < deep_water:
+		return globals.TileType.OCEAN_DEEP
+	elif height < mid_water:
+		return globals.TileType.OCEAN_MID
+	return globals.TileType.OCEAN_SHALLOW
+
+func DetermineLandBiome(height: float, temp: float):
+	# Determine land biome #
+	if temp < tundra:
+		return DetermineColdType(height, temp)
+	elif temp < grass:
+		return DetermineGrassType(height, temp)
+	return DetermineSandType(height, temp)
+
+func DetermineColdType(height: float, temp: float):
+	if temp < snow:
+		return globals.TileType.SNOW
+	if height > mountain:
+		return globals.TileType.SNOW_MOUNTAIN
+	elif height > hill:
+		return globals.TileType.SNOW_HILL
+	return globals.TileType.TUNDRA
+
+func DetermineGrassType(height: float, temp: float):
+	if height > mountain:
+		return globals.TileType.GRASS_MOUNTAIN
+	elif height > hill:
+		return globals.TileType.GRASS_HILL
+	elif randi()%100 < flower_chance:
+		return globals.TileType.GRASS_FLOWER
+	return globals.TileType.GRASS
+
+func DetermineSandType(height: float, temp: float):
+	if height > mountain:
+		return globals.TileType.SAND_MOUNTAIN
+	elif height > hill:
+		return globals.TileType.SAND_HILL
+	return globals.TileType.SAND
 
 func SetMap(world_map: Array):
 	var index = 0
@@ -189,10 +229,18 @@ func _on_button_pressed():
 	# Adjust map size
 	map_size = clamp(map_size_node.value, 10, max_size)
 	# Adjust height map settings
+	deep_water = deep_water_max.value/100.0
+	mid_water = mid_water_max.value/100.0
+	shallow_water = shallow_water_max.value/100.0
+	mountain = mountain_min.value/100.0
+	hill = hill_min.value/100.0
 	height_noise = height_noise_type.value
 	height_fractal = height_fractal_type.value
 	height_frequency = height_freqeuncy_limit.value
 	# Adjust temp map settings
+	snow = snow_max.value/100.0
+	tundra = tundra_max.value/100.0
+	grass = grass_max.value/100.0
 	temp_noise = temp_noise_type.value
 	temp_fractal = temp_fractal_type.value
 	temp_frequency = temp_freqeuncy_limit.value
